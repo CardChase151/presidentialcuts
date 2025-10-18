2251c1bdcad81d9a5e8df153cbaea7678ee6f1af
Chase Kellis <chase@cardchase.org>
2025-10-17 21:29:14 -0700

    Fix Google Maps iframe height to fill container
    - Changed map height from fixed 400px to 100%
    - Added min-height of 500px to prevent map from being too small
    - Map now fills the entire right column matching contact info height
    - Eliminated white space at bottom of map container

--- src/components/Home/Location.jsx
+++ src/components/Home/Location.jsx
@@ -53,7 +53,7 @@ function Location() {
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3318.8!2d-116.4!3d33.78!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80db1d5d1d5d5d5d%3A0x1d5d5d5d5d5d5d5d!2s36101%20Bob%20Hope%20Dr%20STE%20E6%2C%20Rancho%20Mirage%2C%20CA%2092270!5e0!3m2!1sen!2sus!4v1234567890"
               width="100%"
-              height="400"
+              height="100%"
               style={{ border: 0 }}
               allowFullScreen=""
               loading="lazy"
